5114973829a2b952c48a0ea2c62798176b75b231
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-02-07 11:28:17 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 from flask import Flask
+from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "hello, world!"
+
+def application(environ, start_response):
+    response = Response('Hello World!', mimetype='text/plain')
+    return response(environ, start_response)
 
 if __name__ == "__main__":
     app.run()
