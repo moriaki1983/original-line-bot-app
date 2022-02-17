4896a2c9d40471e866890648f9dacbb9252d2a51
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-02-17 12:23:58 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text="/".join(rslt))
+    TextSendMessage(text="/".join(rslt)))
 
 
 # ポート番号の設定
