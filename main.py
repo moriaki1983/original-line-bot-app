dd54cee9e751a64d5a7a5b5888b285ae28e1b2e0
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-02-21 01:09:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #モジュールの読み込み
 import os
-import gunicorn
+#import gunicorn
 import psycopg2
-from flask import Flask,jsonify,request,abort
+from flask import Flask, jsonify, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
