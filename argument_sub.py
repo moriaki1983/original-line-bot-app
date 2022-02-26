1ef97da2f7cd540d97f1ffd387a7244a9505b33b
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-02-27 02:18:35 +0900

    Update argument_sub.py

--- argument_sub.py
+++ argument_sub.py
@@ -19,15 +19,15 @@ def remove_symbol(line_msg_txt):
     rmv_symbl_rslt10 = re.sub("(？)", "", rmv_symbl_rslt9)
 
     #メッセージの中に含まれる英語固有の記号を除去する
-    rmv_symbl_rslt11 = re.sub("(\')", "", rmv_symbl_rslt10)
-    rmv_symbl_rslt12 = re.sub("(\")", "", rmv_symbl_rslt11)
-    rmv_symbl_rslt13 = re.sub("(,)",  "", rmv_symbl_rslt12)
-    rmv_symbl_rslt14 = re.sub("(.)",  "", rmv_symbl_rslt13)
-    rmv_symbl_rslt15 = re.sub("(!)",  "", rmv_symbl_rslt14)
-    rmv_symbl_rslt16 = re.sub("(\?)", "", rmv_symbl_rslt15)
+    #rmv_symbl_rslt11 = re.sub("(\')", "", rmv_symbl_rslt10)
+    #rmv_symbl_rslt12 = re.sub("(\")", "", rmv_symbl_rslt11)
+    #rmv_symbl_rslt13 = re.sub("(,)",  "", rmv_symbl_rslt12)
+    #rmv_symbl_rslt14 = re.sub("(.)",  "", rmv_symbl_rslt13)
+    #rmv_symbl_rslt15 = re.sub("(!)",  "", rmv_symbl_rslt14)
+    #rmv_symbl_rslt16 = re.sub("(\?)", "", rmv_symbl_rslt15)
     
     #メッセージの中に含まれる先頭と末尾の空白と改行を除去する
-    rmv_symbl_rslt_end = rmv_symbl_rslt16.strip()
+    rmv_symbl_rslt_end = rmv_symbl_rslt10.strip()
     return rmv_symbl_rslt_end
 
 
