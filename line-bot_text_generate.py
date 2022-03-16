271e83afeb38a1002874b3328bc992b5bf2dcf66
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-03-17 04:49:23 +0900

    Delete line-bot_text_generate.py

--- line-bot_text_generate.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-
-
-
-#LINEの返信メッセージをインテント、コンテントと、ダイジェストから生成する
-def line_msg_generate_from_intent_content_and_digest(intnt, cntnt, dgst):
-    line_msg_gnrt_frm_intnt_cntnt_and_dgst_rslt = ""
-    return line_msg_gnrt_frm_intnt_cntnt_and_dgst_rslt
