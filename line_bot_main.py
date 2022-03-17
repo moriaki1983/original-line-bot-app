1991256251ce42935e2f20658fc9dc2927297058
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-03-18 01:57:09 +0900

    Add files via upload

--- line_bot_main.py
+++ line_bot_main.py
@@ -143,17 +143,17 @@ def line_msg_analyze(line_msg_txt):
     if extrct_intnt2 != "(その他・不明)":
        line_msg_anlyz_rslt = extrct_intnt2
        return line_msg_anlyz_rslt
-    extrct_intnt3 = line_bot_text_analyze.extract_intent_from_short_and_boilerplate(rmv_symbl)
+    extrct_intnt3 = line_bot_text_analyze.extract_intent_from_short_and_boilerplate(line_msg_txt)
 
     if extrct_intnt3 != "(その他・不明)":
        line_msg_anlyz_rslt = extrct_intnt3
        return line_msg_anlyz_rslt
-    rmv_edprtcl   = line_bot_text_analyze.remove_endparticle(rmv_symbl)
-    extrct_intnt4 = line_bot_text_analyze.extract_intent_from_short_and_boilerplate(rmv_edprtcl)
+    extrct_intnt4 = line_bot_text_analyze.extract_intent_from_short_and_boilerplate(rmv_symbl)
 
     if extrct_intnt4 != "(その他・不明)":
        line_msg_anlyz_rslt = extrct_intnt4
        return line_msg_anlyz_rslt
+    rmv_edprtcl      = line_bot_text_analyze.remove_endparticle(rmv_symbl)
     extrct_intnt_end = line_bot_text_analyze.extract_intent(rmv_edprtcl)
     return extrct_intnt_end
 
