6bdc9dec6fd0bd512074197fbec7ec3955118c97
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-03-19 22:39:19 +0900

    Add files via upload

--- line_bot_main.py
+++ line_bot_main.py
@@ -148,11 +148,13 @@ def line_msg_analyze(line_msg_txt):
     if extrct_intnt3 != "(その他・不明)":
        line_msg_anlyz_rslt = extrct_intnt3
        return line_msg_anlyz_rslt
+    rmv_symbl     = line_bot_text_analyze.remove_symbol(line_msg_txt)
     extrct_intnt4 = line_bot_text_analyze.extract_intent_from_short_and_boilerplate(rmv_symbl)
 
     if extrct_intnt4 != "(その他・不明)":
        line_msg_anlyz_rslt = extrct_intnt4
        return line_msg_anlyz_rslt
+    rmv_symbl        = line_bot_text_analyze.remove_symbol(line_msg_txt)
     rmv_edprtcl      = line_bot_text_analyze.remove_endparticle(rmv_symbl)
     extrct_intnt_end = line_bot_text_analyze.extract_intent(rmv_edprtcl)
     return extrct_intnt_end
