7c13b606ab41379ad54174ba579bbf75d9126a9d
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-03-24 08:45:32 +0900

    Add files via upload

--- line_bot_text_generate.py
+++ line_bot_text_generate.py
@@ -11,6 +11,8 @@ import random
 #ユーザーから送られるLINEメッセージの解析結果を基に、自然でかつ適切な返信メッセージを生成する
 def text_generate_from_analyze_result(line_msg_anlyz_rslt, lsttm_intnt):
     flw_of_uttrnc = line_msg_anlyz_rslt + "→" + lsttm_intnt
+    app.logger.info("flw_of_uttrnc: " + flw_of_uttrnc)
+
     if   flw_of_uttrnc == "(称賛＆礼賛)→":
          rply_msg_lst = ["ありがとうございます", "嬉しいです", "あなたが好きです"]
          txt_gnrt_from_anlyz_rslt = random.choice(rply_msg_lst)
@@ -19,7 +21,7 @@ def text_generate_from_analyze_result(line_msg_anlyz_rslt, lsttm_intnt):
          rply_msg_lst = ["またまた～ お上手ですね", "そう言われても何も出ませんよ"]
          txt_gnrt_from_anlyz_rslt = random.choice(rply_msg_lst)
          cmpltn_flg = True
-    if   flw_of_uttrnc == "(辱め)(卑猥な言動)→":
+    elif flw_of_uttrnc == "(辱め)(卑猥な言動)→":
          rply_msg_lst = ["やめてください", "(/ω＼)ｲﾔﾝ"]
          txt_gnrt_from_anlyz_rslt = random.choice(rply_msg_lst)
          cmpltn_flg = False
