cda849bf537679926ac437fd0c1e6a45a198b012
moriaki1983 <97571216+moriaki1983@users.noreply.github.com>
2022-04-13 21:57:47 +0900

    Add files via upload

--- line_bot_text_generate.py
+++ line_bot_text_generate.py
@@ -12,8 +12,8 @@ from line_bot_character import BotCharacter
 #ユーザーから送られるLINEメッセージの解析結果を基に、自然でかつ適切な返信メッセージを生成する
 def text_generate_from_analyze_result(line_nwrcd, line_oldrcds):
     #ユーザーの発話の流れを示す変数を宣言・定義する
-    flw_of_uttrnc   = line_nwrcd[3]
-    cntnt_of_uttrnc = line_nwrcd[4]
+    flw_of_uttrnc   = line_nwrcd[0][3]
+    cntnt_of_uttrnc = line_nwrcd[0][4]
 
     #ユーザーの発話の流れに沿った返信メッセージを生成する
     if flw_of_uttrnc == "<称賛＆礼賛>":
